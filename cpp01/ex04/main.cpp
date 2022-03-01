0e5cc779134b1c3c17c4937f8c40fadf0264c2b0
Xavier CHALLE <xchalle@e3r2p2.clusters.42paris.fr>
2022-03-01 19:22:54 +0100

    ok

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int	main(int ac, char **av)
 {
 	std::string	content;
 	std::string	newfile;
-	std::ostream	tmp;
 	std::ifstream	_old;
 	std::ofstream	_new; 
+	std::ostringstream o_tmp;
+
 	if (ac != 4)
 	{
 		std::cout << "PROTOTYPE: filename \"stringtoreplace\" \"newstring\"" << std::endl;
 		return (0);
 	}
-	_old.open(av[1], std::ofstream::out | std::ofstream::app);
-	tmp << av[1] << ".replace";
-	tmp >> newfile;
-	_new.open(newfile, std::ifstream::in);
-	_old >> content;
+	_old.open(av[1], std::ifstream::in);
+	o_tmp << av[1] << ".replace";
+	newfile = o_tmp.str();
+	std::cout << newfile << std::endl;
+	_new.open(newfile, std::ofstream::out | std::ofstream::app);
+	content = _old.rdbuf();
 	//std::ofstream av[1] ("test.txt");
-	std::cout <<content << std::endl;
+	std::cout << content << std::endl;
 //	outfile << "my text here!" << std::endl;
 //	av[1].close();
 }
